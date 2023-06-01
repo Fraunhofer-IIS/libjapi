9fd69f0c2755ac74e03881022570b9b864725230
Katja Vornberger <katja.vornberger@iis.fraunhofer.de>
2023-06-01 22:57:23 +0200

    Fix and extend examples included in docs

--- doxydir/demo.cpp
+++ doxydir/demo.cpp
@@ -9,9 +9,26 @@
  * \details
  * This application demonstrates the usage of the JSON API library (libjapi).
  *
- * \copyright
- * Copyright (c) 2018 Fraunhofer IIS.
- * All rights reserved.
+ *\copyright
+ * Copyright (c) 2023 Fraunhofer IIS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 #include <assert.h>
@@ -24,23 +41,32 @@
 #include <japi_pushsrv.h>
 #include <japi_utils.h>
 
+/* Create a resource structure to pass to libjapi*/
+typedef struct resources
+{
+	double temperature;
+}resources;
+
 /*
  * User defined push temperature service routine.
  * Simulates a circular sinus push value.
  */
-int push_temperature(japi_pushsrv_context *psc)
+void push_temperature(japi_pushsrv_context *psc)
 {
 	json_object *jmsg;
 	double i;
 
 	assert(psc != NULL);
 
+	/* Get back pointer on resources (i.e for example sensors values) */
+	resources* sensor_values = (resources*) psc->userptr;
+
 	jmsg = json_object_new_object();
 	while (psc->enabled) {
 
 		for (i = 0.0; i <= 3.14; i += 0.1) {
 			/* Create JSON response string */
-			json_object_object_add(jmsg,"temperature",json_object_new_double(30+10*sin(i)));
+			json_object_object_add(jmsg,"temperature",json_object_new_double(sensor_values->temperature+10*sin(i)));
 
 			/* Push message */
 			japi_pushsrv_sendmsg(psc,jmsg);
@@ -49,12 +75,10 @@ int push_temperature(japi_pushsrv_context *psc)
 
 	}
 	json_object_put(jmsg);
-
-	return 0;
 }
 
 /* User defined push service routine */
-int push_counter(japi_pushsrv_context *psc)
+void push_counter(japi_pushsrv_context *psc)
 {
 	json_object *jmsg;
 	int i;
@@ -75,8 +99,6 @@ int push_counter(japi_pushsrv_context *psc)
 		sleep(1);
 	}
 	json_object_put(jmsg);
-
-	return 0;
 }
 
 static void rnf_handler(japi_context *ctx, json_object *request, json_object *response)
@@ -89,14 +111,15 @@ static void get_temperature(japi_context *ctx, json_object *request, json_object
 	double temperature;
 	const char *unit;
 
-	/*
-	 * TODO: Read the temperature from a sensor...
-	 */
-	temperature = 27.0;
+	/* Get back pointer on resources (i.e for example sensors values) */
+	resources* sensor_values = (resources*) ctx->userptr;
+	temperature = sensor_values->temperature;
 
 	/* Provide the temperature in KELVIN (if requested)
 	 * or CELSIUS (default) */
-	unit = japi_get_value_as_str(request, "unit");
+	if (japi_get_value_as_str(request, "unit", &unit) != 0 ) {
+		fprintf(stderr, "Failed to get string value from key 'unit'\n");
+	}
 	if (unit != NULL && strcmp(unit, "kelvin") == 0) {
 		temperature += 273;
 	} else {
@@ -105,7 +128,6 @@ static void get_temperature(japi_context *ctx, json_object *request, json_object
 
 	/* Prepare and provide response */
 	json_object_object_add(response, "temperature", json_object_new_double(temperature));
-	json_object_object_add(response, "unit", json_object_new_string(unit));
 }
 
 int main(int argc, char *argv[])
@@ -113,6 +135,9 @@ int main(int argc, char *argv[])
 	int ret;
 	japi_context *ctx;
 	japi_pushsrv_context *psc_counter, *psc_temperature;
+
+	/* Declare & initialise resources*/
+	resources temperature_sensor = {17.0};
 
 	/* Read port */
 	if (argc != 2) {
@@ -122,11 +147,14 @@ int main(int argc, char *argv[])
 	}
 
 	/* Create JSON API context */
-	ctx = japi_init(NULL);
+	ctx = japi_init(&temperature_sensor);
 	if (ctx == NULL) {
 		fprintf(stderr, "ERROR: Failed to create japi context\n");
 		return -1;
 	}
+
+	/* Include request args in response */
+	japi_include_args_in_response(ctx, true);
 
 	/* Register JSON API requests */
 	japi_register_request(ctx, "request_not_found_handler", &rnf_handler);
@@ -137,15 +165,14 @@ int main(int argc, char *argv[])
 	psc_temperature = japi_pushsrv_register(ctx, "push_temperature");
 
 	/* Start push threads */
-	japi_pushsrv_start(psc_counter,&push_counter);
-	japi_pushsrv_start(psc_temperature,&push_temperature);
+	japi_pushsrv_start(psc_counter, &push_counter);
+	japi_pushsrv_start(psc_temperature, &push_temperature);
+
+	/* Set maximal number of allowed clients. 0 for unlimited */
+	japi_set_max_allowed_clients(ctx, 3);
 
 	/* Provide JSON API interface via TCP */
 	ret = japi_start_server(ctx, argv[1]);
-
-	/* Wait for the threads to finish */
-	japi_pushsrv_stop(psc_counter);
-	japi_pushsrv_stop(psc_temperature);
 
 	/* Destroy JAPI context */
 	japi_destroy(ctx);
